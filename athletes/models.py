150afc4427cb4b92de4cb60365987e746d93a369
Justin Luther <justinluther@lutherwealth.com>
2021-09-25 17:16:06 -0400

    added Rower model and admin site connection.

--- athletes/models.py
+++ athletes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class Rower(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    height_ft = models.PositiveSmallIntegerField()
+    height_in = models.PositiveSmallIntegerField()
 # Create your models here.
