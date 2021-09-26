8dec90ff6835696142908bf4a29a5f3d08d6de48
Justin Luther <justinluther@lutherwealth.com>
2021-09-26 10:39:33 -0400

    added models and admin for hulls and raced boats.

--- athletes/admin.py
+++ athletes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Rower
+from .models import Rower, Hull, Boat
 
 admin.site.register(Rower)
+admin.site.register(Hull)
+admin.site.register(Boat)
 # Register your models here.
--- athletes/models.py
+++ athletes/models.py
@@ -6,4 +6,49 @@ class Rower(models.Model):
     last_name = models.CharField(max_length=30)
     height_ft = models.PositiveSmallIntegerField()
     height_in = models.PositiveSmallIntegerField()
-# Create your models here.
+    mmr = models.PositiveSmallIntegerField()
+    stroke_mmr = models.PositiveSmallIntegerField()
+
+    class Meta:
+        ordering = ['last_name']
+
+    def __str__(self):
+        return self.last_name + ", " + self.first_name
+
+
+class Hull(models.Model):
+    HULL_MAKES = (
+        ('EMPCH', 'Empacher'),
+        ('MILL', 'Millenium'),
+        ('RESOL', 'Resolute'),
+    )
+    SIZES = (
+        ('S', 'Single'),
+        ('D', 'Double'),
+        ('P', 'Pair'),
+        ('CF', 'Coxed Four'),
+        ('SF', 'Straight Four'),
+        ('Q', 'Quad'),
+        ('E', 'Eight')
+    )
+    make = models.CharField(choices=HULL_MAKES, max_length=30)
+    size = models.CharField(choices=SIZES, max_length=20)
+    year = models.DateTimeField()
+
+    class Meta:
+        ordering = ['year']
+
+    def __str__(self):
+        return self.make + "_" + str(self.year.year)
+
+
+class Boat(models.Model):
+    date = models.DateTimeField()
+    hull = models.ForeignKey(Hull, on_delete=models.CASCADE)
+    crewmember = models.ManyToManyField(Rower)
+
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return self.hull.__str__() + " raced at " + str(self.date)
